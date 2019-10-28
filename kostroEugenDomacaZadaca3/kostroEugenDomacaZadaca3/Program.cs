a21da9fa0ea1cdd2834092c3e618d03658a7345a
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2019-10-28 17:45:12 +0100

    Add files via upload

---
+++ kostroEugenDomacaZadaca3/kostroEugenDomacaZadaca3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kostroEugenDomacaZadaca3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            float ocjene, prosjekOcjena = 0, brojac = 0;
+            Console.WriteLine("Upisi ocjene: ");
+        nazad:
+            ocjene = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+
+                if (ocjene == 1)
+                {
+                    Console.WriteLine("Prosjek ocjena je 1!! ");
+                    goto dalje;
+                }
+                if (ocjene < 1 || ocjene > 5)
+                {
+                    Console.WriteLine("Ocjena ne postoji: Upisi ponovno!");
+                    goto nazad;
+                }
+                prosjekOcjena = prosjekOcjena + ocjene;
+                brojac++;
+                ocjene = Convert.ToInt32(Console.ReadLine());
+            }
+
+            prosjekOcjena = prosjekOcjena / brojac;
+            Console.WriteLine("Ovo je vas prosjek:  " + prosjek);
+
+            dalje:
+
+            Console.ReadKey();
+
+        }
+    }
+}
